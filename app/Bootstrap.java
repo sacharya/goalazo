b2f7a7eff5e88849eff5e883db5ad72d48d47588
Sudarshan Acharya <sudarshanacharya@24-155-118-89.dyn.grandenetworks.net>
2009-10-31 17:54:08 -0500

    Adding css changes

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -15,29 +15,23 @@ public class Bootstrap extends Job {
         	User bob = User.find("byEmail", "bob@gmail.com").first();
         	System.out.println("User: " + bob);
         	
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Who will in the Fifa Player of the Year 2008?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
 
         	
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Is Messi really better than what Maradona was at his age?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
         	
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	new Post(bob, "who is th PM of Nepal?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
-        	
-        	
-        	
+        	new Post(bob, "Where is fat old Ronaldo now?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Will Brazil win it again?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Who is the most overrated player?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "I heard C Ronaldo is gay ", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Samuel Eto hates Guardiola", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "How did Tevez got that scar?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Best soccer player of all time?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Football or Soccer?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Transfer rumors", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Was Ferguson wrong?", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "Most red cards in a game", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save();
+        	new Post(bob, "How much ", "hgfhhfhg hjhjjj hhj hj jh h hjj hjhjhhjh hj hjh jh h ").save(); 	
         	
         //}
     }
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 import models.Post;
 import play.mvc.*;
+import play.*;
 
 public class Application extends Controller {
 
+	@Before
+	static void addDefaults() {
+	    //renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+	
     public static void index() {
     	List<Post> posts = Post.find(
                 "order by createdDate desc"
